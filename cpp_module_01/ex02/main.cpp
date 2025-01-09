77ff1201dcbe998745b4e25e23141f1827d1977a
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-01-09 13:59:56 +0100

    cpp01/ex02 main finish

--- cpp_module_01/ex02/main.cpp
+++ cpp_module_01/ex02/main.cpp
@@ -6,11 +6,24 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 13:26:41 by ade-beco          #+#    #+#             */
-/*   Updated: 2025/01/09 13:26:58 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/01/09 13:40:43 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include <iostream>
+
 int	main( void )
 {
+	std::string	var = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &var;
+	std::string	&stringREF = var;
+
+	std::cout << &var << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << var << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 	return (0);
 }
