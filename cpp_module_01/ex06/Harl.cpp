46f06902ba2995ebc55e74bd9c534fe62d000d55
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-01-16 15:45:36 +0100

    cpp01/ex06 add Harl class and functions

---
+++ cpp_module_01/ex06/Harl.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 14:51:11 by ade-beco          #+#    #+#             */
+/*   Updated: 2025/01/16 15:37:47 by ade-beco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl( void ) {}
+
+Harl::~Harl() {}
+
+
+void	Harl::debug( void )
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info( void )
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning( void )
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error( void )
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now."	<< std::endl;
+}
+
+void	Harl::complain( std::string level )
+{
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void		(Harl::*ptr_complain[4])( void ) =  {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0; i < 4; i++)
+		if (levels[i].compare(level) == 0)
+			(this->*ptr_complain[i])();
+}
