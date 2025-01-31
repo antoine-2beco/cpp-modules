c2b142927cb0b6bdb793c90e85d0a9583ca6dde3
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-01-31 16:17:16 +0100

    cpp02/ex00 little changes

--- cpp_modules_02/ex00/main.cpp
+++ cpp_modules_02/ex00/main.cpp
@@ -6,11 +6,11 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 18:58:15 by ade-beco          #+#    #+#             */
-/*   Updated: 2025/01/29 18:58:26 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/01/31 16:17:01 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Fixed.hpp"
 
 int main( void )
 {
