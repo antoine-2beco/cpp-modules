52c250e4f68f8b8f71e0e21c8d17c0e80596d5bc
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-02-24 15:35:43 +0100

    cpp03/ex01 copy files from prev ex

---
+++ cpp_module_03/ex01/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 15:34:56 by ade-beco          #+#    #+#             */
+/*   Updated: 2025/02/24 15:35:10 by ade-beco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap {
+	
+	public :
+		ClapTrap( std::string name );
+		ClapTrap( const ClapTrap& other );
+		ClapTrap& operator= ( const ClapTrap& other );
+		~ClapTrap();
+		
+		void		attack( const std::string& target );
+		void		takeDamage( unsigned int amount );
+		void		beRepaired( unsigned int amount );
+
+	std::string	_name;
+	int			_hitPoints;
+	int			_energyPoints;
+	int			_attackDamage;
+};
+
+
+#endif
---
+++ cpp_module_03/ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 15:35:01 by ade-beco          #+#    #+#             */
+/*   Updated: 2025/02/24 15:35:13 by ade-beco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main ( void ) {
+	ClapTrap a("Alphonse");
+	ClapTrap b("Brieuc");
+
+	a.attack("Brieuc");
+	b.takeDamage(7);
+
+	b.attack("Alphonse");
+	a.takeDamage(9);
+
+	a.beRepaired(5);
+	b.attack("Alphonse");
+	a.takeDamage(9);
+
+	a.beRepaired(5);
+	b.attack("Alphonse");
+	a.takeDamage(9);
+}
