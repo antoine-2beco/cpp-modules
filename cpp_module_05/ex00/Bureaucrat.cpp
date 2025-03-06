5417e5df2b813c9b710c8b3abde3ba6a755eb1af
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-03-06 12:50:20 +0100

    cpp05/ex00 Bureaucrat class and functions

---
+++ cpp_module_05/ex00/Bureaucrat.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 11:36:57 by ade-beco          #+#    #+#             */
+/*   Updated: 2025/03/06 12:31:11 by ade-beco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+#include <iostream>
+
+Bureaucrat::Bureaucrat( std::string name, int grade ) : _name(name), _grade(grade) {
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat& cpy ) {
+	*this = cpy;
+}
+
+Bureaucrat& Bureaucrat::operator=( const Bureaucrat& cpy ) {
+	if (this != &cpy) {
+		_name = cpy.getName();
+		_grade = cpy.getGrade();
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+const std::string	Bureaucrat::getName() const {
+	return _name;
+}
+
+const int			Bureaucrat::getGrade() const {
+	return _grade;
+}
+
+void	Bureaucrat::incrementGrade() {
+	if (_grade == 1)
+		throw Bureaucrat::GradeTooHighException();
+	_grade--;
+}
+
+void	Bureaucrat::decrementGrade() {
+	if (_grade == 150)
+		throw Bureaucrat::GradeTooLowException();
+	_grade++;
+}
+
+std::ostream&	operator<<( std::ostream& o, const Bureaucrat& src ) {
+	std::cout << src.getName() << ", bureaucrat grade " << src.getGrade() << std::endl;
+	return o;
+}
