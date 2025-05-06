b05c710e677735b47636fd8af83f16facfa16152
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-05-06 16:36:14 +0200

    cpp08/ex01 add tests for span in main.cpp

--- cpp_module_08/ex01/main.cpp
+++ cpp_module_08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:16:06 by ade-beco          #+#    #+#             */
-/*   Updated: 2025/05/06 13:32:46 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/05/06 16:34:45 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,35 @@
 
 int main() {
     Span    a(5);
-    Span    b(a);
     Span    c(10000);
     Span*    d = new Span(111);
-    
+
+
+    for (int i = 0; i < 7; i++) {
+        try {
+            a.addNumber(i);
+        }
+        catch (std::exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+        c.addNumber(i);
+    }
+
+    Span    b(a);
+
+    std::cout << "a : " << a << std::endl;
+    std::cout << "b : " << b << std::endl;
+    std::cout << "c : " << c << std::endl;
+
+    d->addNumber(7);
+    d->addNumber(10);
+    d->addNumber(1);
+    d->addNumber(3);
+
+    std::cout << "d : " << *d << std::endl;
+    std::cout << "shortestSpan in d : " << (*d).shortestSpan() << std::endl;
+    std::cout << "longestSpan in d : " << (*d).longestSpan() << std::endl;
+    std::cout << "d : " << *d << std::endl;
 
     delete (d);
 }
