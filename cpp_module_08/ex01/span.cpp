93e800675203dc445f59bc600d3c4db3835615fd
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-05-06 16:35:52 +0200

    cpp08/ex01 add shortestSpan and longestSpan functions in Span class

--- cpp_module_08/ex01/span.cpp
+++ cpp_module_08/ex01/span.cpp
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:21:50 by ade-beco          #+#    #+#             */
-/*   Updated: 2025/05/06 13:49:10 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/05/06 16:33:55 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,43 @@ Span::Span( const Span& cpy ) {
 Span&    Span::operator=( const Span& cpy ) {
     if (this != &cpy)
         _maximumValue = cpy._maximumValue;
+        list = cpy.list;
     return *this;
 }
 
 Span::~Span() {}
 
 void    Span::addNumber( const int n ) {
-    if (_list.size() == _maximumValue)
+    if (list.size() == _maximumValue)
         throw maximumValueStoredReachedException();
-    _list.push_back(n);
+    list.push_back(n);
+}
+
+int Span::shortestSpan() {
+    int                 span;
+    std::vector<int>    tmp;
+
+    if (list.size() <= 1)
+        throw noSpanCanBeFoundException();
+    tmp = list;
+    std::sort(tmp.begin(), tmp.end());
+    for (std::vector<int>::iterator it = tmp.begin(); (it + 1) != tmp.end(); it++) {
+        if (!span || (*(it + 1) - *it) < span)
+            span = *(it + 1) - *it;
+    }
+    return span;
+}
+
+int Span::longestSpan() {
+    if (list.size() <= 1)
+        throw noSpanCanBeFoundException();
+    return (*std::max_element(list.begin(), list.end()) - *std::min_element(list.begin(), list.end()));
+}
+
+std::ostream&    operator<<( std::ostream& os, Span const &span ) {
+    for (std::vector<int>::const_iterator it = span.list.begin(); it != span.list.end(); it++) {
+        os << " " << *it;
+    }
+    return os;
 }
 
--- cpp_module_08/ex01/span.hpp
+++ cpp_module_08/ex01/span.hpp
@@ -6,18 +6,19 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:17:27 by ade-beco          #+#    #+#             */
-/*   Updated: 2025/05/06 13:48:58 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/05/06 15:43:33 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <iostream>
 # include <vector>
+# include <algorithm>
 
 class Span {
 
-    std::vector<int>     _list;
     unsigned int     _maximumValue;
 
     public:
@@ -26,12 +27,23 @@ class Span {
         Span& operator=( const Span& cpy );
         ~Span();
 
+        std::vector<int>     list;
+
         void    addNumber( const int n );
+        int     shortestSpan();
+        int     longestSpan();
 
         class maximumValueStoredReachedException : public std::exception {
             public :
                 virtual const char* what() const throw() { return "maximum value stored reached"; } ;
         };
+        class noSpanCanBeFoundException : public std::exception {
+            public :
+                virtual const  char* what() const throw() { return "no span can be found"; } ;
+        };
+
 };
+
+std::ostream&   operator<<( std::ostream& os, Span const &span );
 
 #endif
