8a0a7c266a9ca4c82b39eb8459647aaa3a796376
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-05-22 16:05:35 +0200

    cpp09/ex00 add processLine and ValidDate functions

--- cpp_module_09/ex00/BitcoinExchange.cpp
+++ cpp_module_09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 11:47:45 by ade-beco          #+#    #+#             */
-/*   Updated: 2025/05/22 13:44:35 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/05/22 16:04:32 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,11 @@ BitcoinExchange& BitcoinExchange::operator=( const BitcoinExchange &cpy ) {
 
 BitcoinExchange::~BitcoinExchange() {}
 
+
 std::pair<std::string, float>   BitcoinExchange::getData( const std::string &content ) {
-    std::pair<std::string, float>  line;
+    std::pair<std::string, float>   line;
     std::string                     date;
-    float                          value;
+    float                           value;
     char                            *endptr;
     int                             pos;
 
@@ -60,8 +61,96 @@ void BitcoinExchange::readData() {
     ifs.close();
 }
 
+int  BitcoinExchange::isAllDigits(const std::string &s ) {
+    for (long unsigned int i = 0; i < s.length(); i++) {
+        if (s[i] < '0' || s[i] > '9')
+            return (1);
+    }
+    return (0);
+}
+
+int     BitcoinExchange::validDate( const std::string &date ) {
+    std::string s_year = date.substr(0, 3);
+    std::string s_month = date.substr(5, 6);
+    std::string s_day = date.substr(8, 9);
+    char        *endptr;
+
+    if (!isAllDigits(s_year) && !isAllDigits(s_month) && !isAllDigits(s_day)) {
+        std::cerr << "Error : bad input => " << date << std::endl;
+        return (1);
+    }
+
+    int     year = strtol(s_year.c_str(), &endptr, 10);
+    int     month = strtol(s_month.c_str(), &endptr, 10);
+    int     day = strtol(s_day.c_str(), &endptr, 10);
+    int     days;
+
+    if (month < 1 || month > 12) {
+        std::cerr << "Error : bad input => " << date << std::endl;
+        return (1);
+    }
+
+    switch (month) {
+        case 1 : case 3 : case 5 : case 7 : case 8 : case 10 : case 12 :
+            days = 31;
+            break;
+        case 4 : case 6 : case 9 : case 11 :
+            days = 30;
+            break;
+        case 2 :
+            days = 29;
+            if ((year % 400 ==0) && (year % 100 == 0) && (year % 400 == 0))
+                days = 28;
+            break;
+    }
+    if (day > days) {
+        std::cerr << "Error : bad input => " << date << std::endl;
+        return (1);
+    }
+    return (0);
+}
+
+int     BitcoinExchange::validValue( const std::string &value ) {
+    (void) value;
+    return 0;
+}
+
+void    BitcoinExchange::processLine( const std::string &line ) {
+    std::string     date;
+    std::string     s_value;
+    int             pos;
+
+    pos = line.find(" | ");
+    if (pos == static_cast<int>(std::string::npos)) {
+        std::cerr << "Error : bad input => " << line << std::endl;
+        return;
+    }
+    date = line.substr(0, pos);
+    s_value = line.substr(pos + 3, line.length());
+
+    if (validDate(date) || validValue(s_value))
+        return;
+}
+
 void    BitcoinExchange::run( const std::string &dataFile ) {
+    std::ifstream   ifs(dataFile.c_str());
+    std::string     content;
+
     readData();
-    (void)dataFile;
+    if (ifs.is_open()) {
+        if (!getline(ifs, content, '\n')) {
+            std::cerr << "error : " << dataFile << " file is empty" << std::endl;
+            ifs.close();
+            exit(EXIT_FAILURE);
+        }
+        while (getline(ifs, content, '\n')) {
+            processLine(content);
+        }
+    }
+    else {
+        std::cerr << "error : opening " << dataFile << " file failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    ifs.close();
 }
 
--- cpp_module_09/ex00/BitcoinExchange.hpp
+++ cpp_module_09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 11:47:57 by ade-beco          #+#    #+#             */
-/*   Updated: 2025/05/22 13:44:19 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/05/22 15:47:58 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,13 @@ class BitcoinExchange {
 
     std::map<std::string, float>    _data;
 
-    std::pair<std::string, float>   getData( const std::string &content );
+    std::pair<std::string, float>   getData( const std::string &content ); 
     void        readData();
+
+    int         isAllDigits(const std::string &s );
+    int         validDate( const std::string &date );
+    int         validValue( const std::string &value );
+    void        processLine( const std::string &line );
 
     public:
         BitcoinExchange();
