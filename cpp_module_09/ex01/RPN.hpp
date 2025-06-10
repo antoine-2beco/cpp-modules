2c8246148d3af85c327d17250f3dc32d58fbac6b
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-06-10 11:22:33 +0200

    cpp09/ex01 add RPN class, main and Makefile

---
+++ cpp_module_09/ex01/RPN.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/10 11:02:37 by ade-beco          #+#    #+#             */
+/*   Updated: 2025/06/10 11:17:48 by ade-beco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <stack>
+# include <iostream>
+
+class RPN {
+
+    std::stack<std::string>         expressionStack;
+
+    void    expressionParsing( const std::string &expression );
+    void    expressionProcess();
+
+    public :
+        RPN();
+        RPN( const RPN &cpy );
+        const RPN& operator=( const RPN &cpy );
+        ~RPN();
+
+        void    run( const std::string &expression );
+};
+
+#endif
+
