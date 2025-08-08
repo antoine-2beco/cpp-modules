9736206fc03958311220c89da6b606fd7166343a
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2025-08-08 16:45:49 +0200

    cpp09/ex02 add same integer verification

--- cpp_module_09/ex02/main.cpp
+++ cpp_module_09/ex02/main.cpp
@@ -6,23 +6,26 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 14:45:48 by ade-beco          #+#    #+#             */
-/*   Updated: 2025/07/31 17:28:24 by ade-beco         ###   ########.fr       */
+/*   Updated: 2025/08/08 16:44:51 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
 int main( int argc, char *argv[] ) {
-    for ( int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         for (unsigned int j = 0; j < strlen(argv[i]); j++) {
             if (argv[i][j] < '0' || argv[i][j] > '9') {
                 std::cerr << "Error : bad argument : only digits : " << argv[i][j] << std::endl;
                 exit(EXIT_FAILURE);
             }
         }
-        for ( int k = 1; k < argc; k++) {
-            if ( i != k && strcmp(argv[i], argv[k]) == 0 ) {
-                std::cerr << "Error : bad argument : same number : " << argv[i] << std::endl;
+        for (int k = 1; k < argc; k++) {
+            char    *endptr;
+            std::string iNum = argv[i];
+            std::string kNum = argv[k];
+            if ( i != k && strtoll(iNum.c_str(), &endptr, 10) == strtoll(kNum.c_str(), &endptr,  10) ) {
+                std::cerr << "Error : bad argument : same numbers : " << argv[i] << " == " << argv[k] << std::endl;
                 exit(EXIT_FAILURE);
             }
         }
